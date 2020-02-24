a3a871da1ca9de855c7865261411cf493e376c7b
raculber <rculbert5678@gmail>
2020-02-24 09:55:30 -0600

    More changes to employee detail html and js

--- RegisterApp-Java/src/main/resources/static/scripts/employeeDetail.js
+++ RegisterApp-Java/src/main/resources/static/scripts/employeeDetail.js
@@ -1,10 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("save").addEventListener("click", saveEmployee);
+    document.getElementById("signOutImage").addEventListener("click", signOut);
 });
 function saveEmployee() {
+    alert("saved!");
     if (!validateInput()) {
         return;
     }
+    document.getElementById("employeeSavedAlertModal").style.display="block";
     const requestPayload = {
         firstName = document.getElementById("firstName").value,
         lastName = document.getElementById("lastName").value,
@@ -18,25 +21,28 @@ function saveEmployee() {
 }
 function validateInput() {
     if (document.getElementById("firstName").value == "") {
-        displayError("Error: Invalid Input");
+        displayError("Error: Invalid First Name");
         return false;
     }
     else if (document.getElementById("lastName").value == "") {
-        displayError("Error: Invalid Input");
+        displayError("Error: Invalid Last Name");
         return false;
     }
     else if (document.getElementById("password").value == "" || document.getElementById("confirmPassword").value !=
      document.getElementById("password").value) {
-        displayError("Error: Invalid Input");
+        displayError("Error: Invalid Password");
          return false;
      }
      else if (document.getElementById("empType").value != "Cashier" && 
      document.getElementById("empType").value != "Shift Manager" &&
      document.getElementById("empType").value != "General Manager") {
-         displayError("Error: Invalid Input");
+         displayError("Error: Invalid Employee Type");
          return false;
      }
      else {
          return true;
      }
+}
+function signOut() {
+    window.location.replace("/signOut");
 }
